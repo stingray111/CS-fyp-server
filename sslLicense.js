12cc59cc0970d561de1fea0ae76e951e382c3f54
stingray111 <pingshan1013@hotmail.com>
2017-02-15 22:40:10 +0000

    Firebase support

---
+++ sslLicense.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+//ssl license
+
+var keyPath = 'ssl/key.pem';
+var certPath = 'ssl/cert.pem';
+
+var hskey = fs.readFileSync(keyPath);
+var hscert = fs.readFileSync(certPath);
+
+var options = {
+    key: hskey,
+    cert: hscert
+};
+
+//ssl object
+
+var ssl = {};
+
+ssl.options = options;
+
+module.exports = ssl;
