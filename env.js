da38a3e7886721c976ddb80994beec57c6cea4cf
ken31ee <tungpakyin04@outlook.com>
2017-03-16 16:20:43 +0800

    Update using google cloudSQL

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 module.exports = {
     DB_TYPE: 'postgres',   //'mysql'|'sqlite'|'postgres'|'mssql',
-    DB_HOST: 'cs-fyp.cmn6fd5mh09e.ap-southeast-1.rds.amazonaws.com',
-    DB_NAME: 'CS_FYP',        //database schema name
-    DB_USERNAME: 'root',
+    DB_HOST: '35.185.143.3',
+    DB_NAME: 'csfyp',        //database schema name
+    DB_USERNAME: 'postgres',
     DB_PASSWORD: 'wysiwyg.'
 };
